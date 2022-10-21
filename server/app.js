7609e806b63dab15ab34d1d1693631b4f69c8d99
Ruben Perez <ruben.perezfeijoo@ceterus.com>
2022-10-21 11:21:59 -0400

    add graphi

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
@@ -7,7 +8,9 @@ const app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
-    // pass in a schema property
+    schema,
+    // to test and use GraphiQL
+    graphiql: true,
   })
 );
 
