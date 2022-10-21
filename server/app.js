38db69ba7ff6e011e376190cc35fbe9e8ec3cf0a
Ruben Perez <ruben.perezfeijoo@ceterus.com>
2022-10-21 10:34:10 -0400

    start project with graphql

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
+
+// bind express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // pass in a schema property
+  })
+);
+
+app.listen(4000, () => {
+  console.log("now listening for requests on port 4000");
+});
