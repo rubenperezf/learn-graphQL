09d84de61d98ef7a9976c13e347aca16f4cff240
Ruben Perez <ruben.perezfeijoo@ceterus.com>
2022-10-24 11:34:04 -0400

    connect with mongo db

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.im5lw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("graphql").collection("books");
+  // perform actions on the collection object
+  console.log("here", collection)
+  client.close();
+});
 
 const app = express();
 
