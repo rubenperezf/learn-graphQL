bd3e19def6da7a94b0f6bd0daf976d5b9218cd5b
Ruben Perez <ruben.perezfeijoo@ceterus.com>
2022-10-24 10:16:20 -0400

    add lists

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -73,6 +73,18 @@ const RootQuery = new GraphQLObjectType({
         return _.find(authors, { id: args.id });
       },
     },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      },
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
+      },
+    },
   },
 });
 
